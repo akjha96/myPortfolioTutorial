0b5fcf6de178cfd5e4cf9a94deb22e39d55501d1
akjha96 <akjha96@gmail.com>
2020-08-16 05:01:45 +0530

    added about section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
+import { Parallax } from "react-parallax";
+import Container from "react-bootstrap/Container";
 
 // components
 import MyNavbar from "./components/my-navbar/mynavbar.component";
 import MyCarousal from "./components/my-carousal/my-carousal.component";
 import TitleMessage from "./components/title-message/title-message.component";
+import About from "./pages/about/about.component";
 
 import "./App.css";
 
@@ -13,6 +17,16 @@ const App = () => {
       <MyNavbar />
       <MyCarousal />
       <TitleMessage />
+      {/* about me section */}
+      <div>
+        <Parallax blur={{ min: -1000, max: 1000 }} bgImage={require("./assets/img/parallex/background.webp")} bgImageAlt="" strength={-300}>
+          <Container className="container-box rounded">
+            <Fade duration={500}>
+              <About />
+            </Fade>
+          </Container>
+        </Parallax>
+      </div>
     </div>
   );
 };
